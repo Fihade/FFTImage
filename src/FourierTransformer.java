c01cdb02749c208250ce2b49caab5fefbb458dc0
MillerLiang <841985944@qq.com>
2018-06-10 01:39:31 +0800

    add smallImage

--- src/FourierTransformer.java
+++ src/FourierTransformer.java
@@ -1,10 +1,10 @@
-import java.awt.Graphics;
-import java.awt.Image;
+import java.awt.*;
 import java.awt.image.BufferedImage;
 import java.awt.image.ColorModel;
 import java.awt.image.PixelGrabber;
 import java.awt.image.WritableRaster;
 import java.io.File;
+import java.io.FileNotFoundException;
 import java.io.IOException;
 
 import javax.imageio.ImageIO;
@@ -13,17 +13,17 @@ import javax.swing.JFrame;
 @SuppressWarnings("serial")
 public class FourierTransformer  extends JFrame{
     Image im;
-    int i;
     BufferedImage imageAuth = null;
     int iw;
     int ih;
     int[] pixels;
     int[] newPixels;
-
+    File old = new File("C:/Users/swaggymiller/Desktop/2.jpg");
+    File neew = new File("C:/Users/swaggymiller/Desktop/2-test.jpg");
     public FourierTransformer() {
-
+        String zipImage = zipImageFile(old,neew,200,100,0.7f);
         try {
-            this.im = ImageIO.read(new File("C:/Users/swaggymiller/Desktop/1.jpg"));
+            this.im = ImageIO.read(new File(zipImage));
         } catch (IOException e1) {
             e1.printStackTrace();
         }
@@ -139,7 +139,7 @@ public class FourierTransformer  extends JFrame{
         imageAuth.setData(raster);
 
         try {
-            ImageIO.write(imageAuth, "bmp", new File("fft_result.bmp"));
+            ImageIO.write(imageAuth, "png", new File("fftimage.png"));
         } catch (IOException e) {
             e.printStackTrace();
         }
@@ -147,5 +147,61 @@ public class FourierTransformer  extends JFrame{
 
 
         return imageAuth;
+    }
+    /**
+     * 根据设置的宽高等比例压缩图片文件<br> 先保存原文件，再压缩、上传
+     * @param oldFile  要进行压缩的文件
+     * @param newFile  新文件
+     * @param width  宽度 //设置宽度时（高度传入0，等比例缩放）
+     * @param height 高度 //设置高度时（宽度传入0，等比例缩放）
+     * @param quality 质量
+     * @return 返回压缩后的文件的全路径
+     */
+    public static String zipImageFile(File oldFile,File newFile, int width, int height,float quality) {
+        if (oldFile == null) {
+            return null;
+        }
+        try {
+            /** 对服务器上的临时文件进行处理 */
+            Image srcFile = ImageIO.read(oldFile);
+            int w = srcFile.getWidth(null);
+            int h = srcFile.getHeight(null);
+            double bili;
+            if(width>0){
+                bili=width/(double)w;
+                height = (int) (h*bili);
+            }else{
+                if(height>0){
+                    bili=height/(double)h;
+                    width = (int) (w*bili);
+                }
+            }
+
+            String srcImgPath = newFile.getAbsoluteFile().toString();
+            System.out.println(srcImgPath);
+            String subfix = "jpg";
+            subfix = srcImgPath.substring(srcImgPath.lastIndexOf(".")+1,srcImgPath.length());
+
+            BufferedImage buffImg = null;
+            if(subfix.equals("png")){
+                buffImg = new BufferedImage(width, height, BufferedImage.TYPE_INT_ARGB);
+            }else{
+                buffImg = new BufferedImage(width, height, BufferedImage.TYPE_INT_RGB);
+            }
+
+            Graphics2D graphics = buffImg.createGraphics();
+            graphics.setBackground(new Color(255,255,255));
+            graphics.setColor(new Color(255,255,255));
+            graphics.fillRect(0, 0, width, height);
+            graphics.drawImage(srcFile.getScaledInstance(width, height, Image.SCALE_SMOOTH), 0, 0, null);
+
+            ImageIO.write(buffImg, subfix, new File(srcImgPath));
+
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        return newFile.getAbsolutePath();
     }
 }
